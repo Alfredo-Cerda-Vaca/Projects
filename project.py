fd43c06f59b032e6bc6eb085f35b749b70983ed3
Alfredo-Cerda-Vaca <josealfredocv.24@gmail.com>
2023-05-11 01:50:43 +0200

    añadir proyecto

---
+++ project.py
@@ -0,0 +1,9 @@
+#!C:\Users\Alfredo Cerda\Desktop\Projects\env\Scripts\python.exe
+# Author: Alfredo Cerda Vaca (josealfredocv24@gmail.com)
+# Copyright (c) 2023-2023 Guadalajara Jalisco.
+# Project PDO System 
+
+
+
+
+
